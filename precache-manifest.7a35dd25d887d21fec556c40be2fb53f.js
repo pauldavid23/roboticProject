1026aaae0313dba43845db02a188bd2d4116bb24
pauldavid23 <50937998+pauldavid23@users.noreply.github.com>
2020-06-07 21:19:57 +0800

    Updates

---
+++ precache-manifest.7a35dd25d887d21fec556c40be2fb53f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f3291f8162ab5788bc5b6741ef0e22",
+    "url": "/pauldavid23/roboticProject/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/pauldavid23/roboticProject/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8c45ff9f43768718c3c8",
+    "url": "/pauldavid23/roboticProject/static/css/main.335f4b20.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/pauldavid23/roboticProject/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pauldavid23/roboticProject/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c45ff9f43768718c3c8",
+    "url": "/pauldavid23/roboticProject/static/js/main.e5387f23.chunk.js"
+  },
+  {
+    "revision": "e15c556c54275a4f7f44",
+    "url": "/pauldavid23/roboticProject/static/js/runtime-main.3e85b04f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/pauldavid23/roboticProject/static/media/SEGA.12a3db8b.woff"
+  }
+]);
